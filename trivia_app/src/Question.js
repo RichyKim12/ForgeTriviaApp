d8077d9d98061994836b9bab525ed9b202a1c8ed
RichyKim12 <rmk9ds@virginia.edu>
2023-04-15 12:44:24 -0400

    Completed
    Followed along during lecture to complete the project

--- trivia_app/src/Question.js
+++ trivia_app/src/Question.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
 function Question({question, correct, incorrect}){
-    const allAnswers=[...incorrect, correct];
+    const [shuffledAnswers, setShuffledAnswers] = useState([]);
+    
+    useEffect(() => {
+        const allAnswers=[...incorrect, correct];
+        setShuffledAnswers(allAnswers.sort(() => Math.random() - 0.5))
+    }, [correct,incorrect]);
 
+    // Give feedback on whether answer clicked is correct
+    const [clickedAnswer,setClickedAnswer] = useState(null);
+    function checkIfCorrect(answer){
+        if (answer !== clickedAnswer){
+            return '';
+        }
+        else{
+            if (answer === correct){
+                return 'green';
+            }
+            else{
+                return 'red';
+            }
+        }
+    }
+
+    function handleAnswerClick(answer){
+        if(clickedAnswer === null){
+            setClickedAnswer(answer);
+        }
+    }
     return(
         <div>
             <h2>{question}</h2>
             <ul>
-                # this stuff is new
-                {allAnswers.map((answer,index) => (
+                
+                {shuffledAnswers.map((answer,index) => (
                     <li key = {index}>
-                        <button> {answer}</button>
+                        <Button 
+                        variant = 'contained'
+                        style = {{backgroundColor: checkIfCorrect(answer)}} 
+                        onClick={() => handleAnswerClick(answer)}
+                        disabled={clickedAnswer!==null}> 
+                        {answer}
+                        </Button>
                     </li>
                 )
                 )}
